6000b09d12cd64ae0f8676f128d527814e46929f
Ernesto <ernesto7816@gmail.com>
2024-05-20 18:44:37 -0500

    commit principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { BibliotecaComponent } from './biblioteca/biblioteca.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:InicioComponent},
+    {path:"inicio",component:InicioComponent},
+    {path:"biblioteca",component:BibliotecaComponent}
+];
